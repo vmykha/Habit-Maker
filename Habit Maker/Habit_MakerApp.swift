9918519d35b6b5ea11be913c2d653951a6faf4dc
Volodymyr Myhailyuk <volodymyr.myhailyuk@eleks.com>
2022-07-05 15:26:45 +0300

    Initial Commit
    initial commit

---
+++ Habit Maker/Habit_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_MakerApp.swift
+//  Habit Maker
+//
+//  Created by Volodymyr Myhailyuk on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Habit_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
